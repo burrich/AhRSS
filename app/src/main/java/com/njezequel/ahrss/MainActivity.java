afb47420a19c099e4e6c2c843effda1b8d6e8a86
burrich <jezequel.n@gmail.com>
2015-12-15 21:55:43 +0100

    RssListFragment avec des données statiques

--- app/src/main/java/com/njezequel/ahrss/MainActivity.java
+++ app/src/main/java/com/njezequel/ahrss/MainActivity.java
@@ -1,13 +1,15 @@
 package com.njezequel.ahrss;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleAdapter;
+
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Initialisation de la listview de RssListFragment
+
+        ListFragment rssListFragment = (ListFragment) getSupportFragmentManager().findFragmentById(
+                R.id.rss_list_fragment
+        );
+        List<Map<String, String>> data = RssListFragment.generateData();
+
+        // Mapping clés / view des éléments d'une ligne
+        String[] viewsKeys = {"1", "2"};
+        int[] views = {R.id.text1_rss_list, R.id.text2_rss_list};
+
+        // Adapter
+        SimpleAdapter rssListAdapter = new SimpleAdapter(
+                this, data, R.layout.item_rss_list, viewsKeys, views
+        );
+        rssListFragment.setListAdapter(rssListAdapter);
     }
 
     @Override
